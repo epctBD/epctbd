7103d0dc366497b5edd73134a9149d4dadbe5c54
dkn1ght23 <mujammal.salman@gmail.com>
2024-11-08 20:01:25 +0600

    Update .gitignore

--- client-side/src/components/layout/footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  return <div style={{ padding: "20px", backgroundColor: "gray" }}>Footer</div>;
-};
-
-export default Footer;
--- client-side/src/components/layout/navbar/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Navbar = () => {
-  return <div style={{ padding: "20px", backgroundColor: "gray" }}>Navbar</div>;
-};
-
-export default Navbar;
