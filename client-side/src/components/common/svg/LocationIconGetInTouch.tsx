7d75f15468b28e19abd74102705245b1a25dcf67
Raisul Karim Saju <rksaju80@gmail.com>
2024-12-10 13:38:32 +0600

    get in touch added

---
+++ client-side/src/components/common/svg/LocationIconGetInTouch.tsx
@@ -0,0 +1,27 @@
+const LocationIconGetInTouch = () => {
+  return (
+    <svg
+      width="47"
+      height="46"
+      viewBox="0 0 47 46"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.16797 19.4412C8.16797 10.8212 15.0329 3.83325 23.5013 3.83325C31.9697 3.83325 38.8346 10.8212 38.8346 19.4412C38.8346 27.9937 33.9408 37.9737 26.3053 41.5426C24.5253 42.3746 22.4773 42.3746 20.6973 41.5426C13.0618 37.9737 8.16797 27.9937 8.16797 19.4412Z"
+        stroke="#0077EE"
+        stroke-width="1.5"
+      />
+      <ellipse
+        cx="23.5"
+        cy="19.1667"
+        rx="5.75"
+        ry="5.75"
+        stroke="#0077EE"
+        stroke-width="1.5"
+      />
+    </svg>
+  );
+};
+
+export default LocationIconGetInTouch;
