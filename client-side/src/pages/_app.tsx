9db66a75886b4f88089facfcab8cc787340e46c0
dkn1ght23 <mujammal.salman@gmail.com>
2024-12-11 04:03:17 +0600

    Font Added

--- client-side/src/pages/_app.tsx
+++ client-side/src/pages/_app.tsx
@@ -3,19 +3,27 @@ import "../styles/global.scss";
 import type { AppProps } from "next/app";
 import theme from "./theme/themeConfig";
 import { ConfigProvider } from "antd";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function App({ Component, pageProps, router }: AppProps) {
   const isAdminRoute = router.pathname.startsWith("/admin");
 
   return (
     <ConfigProvider theme={theme}>
-      {isAdminRoute ? (
-        <Component {...pageProps} />
-      ) : (
-        <LayoutWrapper>
+      <div className={inter.className}>
+        {isAdminRoute ? (
           <Component {...pageProps} />
-        </LayoutWrapper>
-      )}
+        ) : (
+          <LayoutWrapper>
+            <Component {...pageProps} />
+          </LayoutWrapper>
+        )}
+      </div>
     </ConfigProvider>
   );
 }
