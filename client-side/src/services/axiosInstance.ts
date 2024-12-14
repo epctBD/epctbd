b8e18fbf3bc43987dcfdd9521a6a1dbcfae20077
dkn1ght23 <mujammal.salman@gmail.com>
2024-12-15 01:07:13 +0600

    local env changed

--- client-side/src/services/axiosInstance.ts
+++ client-side/src/services/axiosInstance.ts
@@ -1,8 +1,13 @@
 // services/apiService.ts
 import Axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://epct-production.up.railway.app/api/" // Production URL
+    : "http://localhost:5000/api/"; // Local URL
+
 const AXIOS_INSTANCE = Axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL,
   withCredentials: true,
 });
 
