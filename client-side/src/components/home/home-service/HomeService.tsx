318a29c18e13597523fcc8ff40af2b4c9dfa6d28
dkn1ght23 <mujammal.salman@gmail.com>
2024-12-17 02:35:25 +0600

    build issue fixed

--- client-side/src/components/home/home-service/HomeService.tsx
+++ client-side/src/components/home/home-service/HomeService.tsx
@@ -15,12 +15,12 @@ const HomeService = () => {
       </div>
       <Row gutter={[24, 24]} justify="center" align="middle">
         {services.map((service) => (
-          <Col xs={24} sm={12} md={8} lg={6} key={service.slug}>
+          <Col xs={24} sm={12} md={8} lg={6} key={service.serviceSlug}>
             <ServiceCard
               image={service.image}
               title={service.title}
               subtitle={service.description}
-              slug={service.slug}
+              slug={service.serviceSlug}
             />
           </Col>
         ))}
