c79001530caf5bf9264736d7ddcf0e02d29c40fe
Raisul Karim Saju <rksaju80@gmail.com>
2024-12-18 14:33:19 +0600

    marquee responsive done

--- client-side/src/components/about-us/decade-section-view/DecadeSectionView.tsx
+++ client-side/src/components/about-us/decade-section-view/DecadeSectionView.tsx
@@ -6,9 +6,13 @@ import DecadeCard from "@/components/common/decade-card/DecadeCard";
 const DecadeSectionView = () => {
   return (
     <div className={styles.decadeSectionViewWrapper}>
-      <div className={styles.startTag}>
-        <div className={styles.divider} />
-        <p className={styles.tagText}>WHO WE ARE</p>
+      <div>
+        <div className={styles.startTag}>
+          <div className={styles.divider} />
+          <p className={styles.tagText}>OUR HISTORY</p>
+        </div>
+
+        <p className={styles.tagTitle}>Our History at a Glance</p>
       </div>
       <div className={styles.marqueeWrapper}>
         <Marquee direction="left" pauseOnHover={true} speed={100}>
