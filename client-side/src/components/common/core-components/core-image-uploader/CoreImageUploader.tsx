a4abeee7619e3c98d35a2a58f3c06abfda038837
dkn1ght23 <mujammal.salman@gmail.com>
2025-01-08 23:57:50 -0500

    core upploader editing

--- client-side/src/components/common/core-components/core-image-uploader/CoreImageUploader.tsx
+++ client-side/src/components/common/core-components/core-image-uploader/CoreImageUploader.tsx
@@ -1,5 +1,5 @@
-import { Image, message } from "antd";
 import React, { useRef, ChangeEvent, useState } from "react";
+import { Image, message } from "antd";
 import CoreButton from "../core-button/CoreButton";
 
 interface ImageUploaderProps {
@@ -22,72 +22,40 @@ const CoreImageUploader: React.FC<ImageUploaderProps> = ({
   imageB64,
 }) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [, setImage] = useState<File | null>(null);
+  const [image, setImage] = useState<File | null>(null);
 
   const isValidImage = (file: File): boolean => {
     const validTypes = ["image/jpeg", "image/png", "image/gif"];
     if (!validTypes.includes(file.type)) {
       message.error(
-        "Invalid image type. Please upload a JPG, JPEG or PNG image."
+        "Invalid image type. Please upload a JPG, JPEG, or PNG image."
       );
       return false;
     }
     return true;
   };
 
-  const isValidDimensions = (file: File): Promise<boolean> => {
-    return new Promise<boolean>((resolve) => {
-      const img = new (window as Window & typeof globalThis).Image();
-
-      img.onload = () => {
-        if (img.width < 1680 || img.height < 1366) {
-          message
-            .error(
-              "Invalid image dimensions. Please upload an image with dimensions 680x366 or larger."
-            )
-            .then(() => {
-              resolve(false);
-            });
-        } else {
-          resolve(true);
-        }
-      };
-
-      img.src = URL.createObjectURL(file);
-    });
-  };
-
   const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
     const fileObj = event.target.files && event.target.files[0];
-
-    if (!fileObj) {
+    if (!fileObj || !isValidImage(fileObj)) {
       return;
     }
-
-    if (!isValidImage(fileObj)) {
-      return;
-    }
-
-    // const dimensionsValid = await isValidDimensions(fileObj);
-    // if (!dimensionsValid) {
-    //   return;
-    // }
 
     setImage(fileObj);
     onFileChange(fileObj);
 
-    const selectedImage = fileObj;
     const reader = new FileReader();
-
     reader.onloadend = () => {
       onLoadEnd(reader.result as string);
     };
 
-    if (selectedImage) {
-      reader.readAsDataURL(selectedImage);
-    }
-    event.target.value = "";
+    reader.readAsDataURL(fileObj);
+    event.target.value = ""; // Clear input value to allow re-upload
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null);
+    onFileChange(null);
   };
 
   const handleUploadClick = () => {
@@ -97,38 +65,39 @@ const CoreImageUploader: React.FC<ImageUploaderProps> = ({
   };
 
   return (
-    <div style={{ display: "flex" }}>
-      <CoreButton
-        type="primary"
-        text={buttonText}
-        onClick={handleUploadClick}
-      />
+    <div style={{ display: "flex", alignItems: "center" }}>
+      {!image && !imageB64 && (
+        <CoreButton
+          type="primary"
+          text={buttonText}
+          onClick={handleUploadClick}
+        />
+      )}
 
       <input
         ref={inputRef}
         style={{ display: "none" }}
         type="file"
+        accept="image/*"
         onChange={handleFileChange}
       />
 
-      {exsistingFile && !imageB64 && (
-        <div style={{ display: "flex" }}>
+      {(imageB64 || image) && (
+        <div
+          style={{ display: "flex", alignItems: "center", marginLeft: "16px" }}
+        >
           <Image
             width={imageWidth}
             height={imageHeight}
-            src={exsistingFile}
+            src={imageB64 || URL.createObjectURL(image!)}
             alt="Uploaded Image"
+            style={{ marginRight: "8px" }}
           />
-        </div>
-      )}
-
-      {imageB64 && (
-        <div style={{ display: "flex" }}>
-          <Image
-            width={imageWidth}
-            height={imageHeight}
-            src={imageB64}
-            alt="Uploaded Image"
+          <CoreButton
+            // type="text"
+            text="Remove"
+            onClick={handleRemoveImage}
+            // style={{ color: "red" }}
           />
         </div>
       )}
