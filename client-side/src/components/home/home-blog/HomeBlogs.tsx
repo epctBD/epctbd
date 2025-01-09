aec14183f620c5f817252fdad11cb62f4a7a367b
Raisul Karim Saju <rksaju80@gmail.com>
2025-01-09 23:58:52 +0600

    see more button addes in blog home

--- client-side/src/components/home/home-blog/HomeBlogs.tsx
+++ client-side/src/components/home/home-blog/HomeBlogs.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Button } from "antd";
 import CoreTitles from "@/components/common/core-titles/CoreTitles";
 import BlogCard from "@/components/common/blog-card/BlogCard";
 import { blogData } from "./HomeBlogData";
 import styles from "./HomeBlogs.module.scss";
+import { useRouter } from "next/router";
 
 const HomeBlogs = () => {
+  const router = useRouter();
+  const goToDetails = () => {
+    router.push("resources");
+  };
+
   return (
     <div className={styles.HomeBlogWrapper}>
       <CoreTitles
@@ -14,16 +20,34 @@ const HomeBlogs = () => {
         intro="Dive into our collection of insightful articles, industry trends, and expert opinions."
       />
 
+      <div className={styles.ButtonSeeMoreWrapper}>
+        <Button
+          variant="text"
+          style={{
+            color: "#0077EE",
+            backgroundColor: "transparent",
+            padding: "5px 10px",
+            border: "none",
+            fontSize: "18px",
+            fontWeight: "600",
+          }}
+          onClick={goToDetails}
+        >
+          See More
+        </Button>
+      </div>
       <Row gutter={[16, 16]} className={styles.blogsContainer}>
         {blogData.slice(0, 3).map((blog) => (
           <Col key={blog.id} xs={24} sm={12} md={12} xl={8}>
-            <BlogCard
-              slug={blog.title}
-              title={blog.title}
-              content={blog.description}
-              image={blog.image}
-              tag={blog.type}
-            />
+            <div className={styles.blogsInnerContainer}>
+              <BlogCard
+                slug={blog.title}
+                title={blog.title}
+                content={blog.description}
+                image={blog.image}
+                tag={blog.type}
+              />
+            </div>
           </Col>
         ))}
       </Row>
