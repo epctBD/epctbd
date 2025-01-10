f156eda55ce0cff7fb3d092c26aaa48072e6ab31
Raisul Karim Saju <rksaju80@gmail.com>
2025-01-10 12:44:21 +0600

    home blogs from backend added

--- client-side/src/components/home/home-blog/HomeBlogs.tsx
+++ client-side/src/components/home/home-blog/HomeBlogs.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Row, Col, Button } from "antd";
 import CoreTitles from "@/components/common/core-titles/CoreTitles";
 import BlogCard from "@/components/common/blog-card/BlogCard";
-import { blogData } from "./HomeBlogData";
 import styles from "./HomeBlogs.module.scss";
 import { useRouter } from "next/router";
+import { IBlog } from "@/models/blog.model";
 
-const HomeBlogs = () => {
+interface IHomeBlogsProps {
+  blogs: IBlog[];
+}
+
+const HomeBlogs = ({ blogs }: IHomeBlogsProps) => {
   const router = useRouter();
+
   const goToDetails = () => {
     router.push("resources");
   };
@@ -20,32 +25,33 @@ const HomeBlogs = () => {
         intro="Dive into our collection of insightful articles, industry trends, and expert opinions."
       />
 
-      <div className={styles.ButtonSeeMoreWrapper}>
-        <Button
-          variant="text"
-          style={{
-            color: "#0077EE",
-            backgroundColor: "transparent",
-            padding: "5px 10px",
-            border: "none",
-            fontSize: "18px",
-            fontWeight: "600",
-          }}
-          onClick={goToDetails}
-        >
-          See More
-        </Button>
-      </div>
       <Row gutter={[16, 16]} className={styles.blogsContainer}>
-        {blogData.slice(0, 3).map((blog) => (
-          <Col key={blog.id} xs={24} sm={12} md={12} xl={8}>
+        <Col span={24}>
+          <div className={styles.ButtonSeeMoreWrapper}>
+            <Button
+              variant="text"
+              style={{
+                color: "#0077EE",
+                backgroundColor: "transparent",
+                border: "none",
+                fontSize: "18px",
+                fontWeight: "600",
+              }}
+              onClick={goToDetails}
+            >
+              See More
+            </Button>
+          </div>
+        </Col>
+        {blogs.slice(0, 3).map((blog) => (
+          <Col key={blog.slug} xs={24} sm={12} md={12} xl={8}>
             <div className={styles.blogsInnerContainer}>
               <BlogCard
-                slug={blog.title}
+                slug={blog.slug}
                 title={blog.title}
-                content={blog.description}
-                image={blog.image}
-                tag={blog.type}
+                content={blog.content}
+                image={blog.thumbnail}
+                tag={blog.tag}
               />
             </div>
           </Col>
