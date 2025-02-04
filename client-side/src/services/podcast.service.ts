3218dd0a1283e887e2b8db967aed6948e8d9134b
Raisul Karim Saju <rksaju80@gmail.com>
2025-02-04 21:15:28 +0600

    podcast service and model created

---
+++ client-side/src/services/podcast.service.ts
@@ -0,0 +1,19 @@
+import { IPodcast, IPodcastResponse } from "@/models/podcast.model";
+import AXIOS_INSTANCE from "./axiosInstance";
+
+export const getPodcasts = async (): Promise<IPodcast[]> => {
+  const response = await AXIOS_INSTANCE.get<IPodcastResponse>("podcast");
+  return response?.data?.data;
+};
+
+export const addPodcast = async (data: FormData): Promise<IPodcast[]> => {
+  const response = await AXIOS_INSTANCE.post<IPodcastResponse>("podcast", data);
+  return response?.data?.data;
+};
+
+export const deletePodcast = async (id: string): Promise<IPodcast[]> => {
+  const response = await AXIOS_INSTANCE.delete<IPodcastResponse>(
+    `/podcast/${id}`
+  );
+  return response?.data?.data;
+};
